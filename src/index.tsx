95391b3a0c4e631f41373f36737bd19d45621ba7
sundr17 <sundoor1992@mail.ru>
2024-01-23 16:45:13 +0100

    Change BrowserRouter to HashRouter for gh-pages using

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { WagmiProvider } from 'wagmi';
@@ -20,13 +20,13 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
+      <HashRouter>
         <WagmiProvider config={wagmiConfig}>
           <QueryClientProvider client={queryClient}>
             <App />
           </QueryClientProvider>
         </WagmiProvider>
-      </BrowserRouter>
+      </HashRouter>
     </Provider>
   </React.StrictMode>
 );
