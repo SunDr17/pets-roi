4391aaa97ce4709e3eb4373643cb0b79d44d593b
sundr17 <sundoor1992@mail.ru>
2024-01-24 11:20:54 +0100

    Add inflections for strings

--- src/components/pages/Tokenomics/index.tsx
+++ src/components/pages/Tokenomics/index.tsx
@@ -3,14 +3,25 @@ import { useTranslation } from 'react-i18next';
 import Table from 'react-bootstrap/Table';
 
 import config from '@/config';
+import { getWordInflection } from '@/utils/string';
 
 export default function Tokenomics() {
   const { t } = useTranslation();
+  const cycleDuration = config.cycleDuration / (60 * 60 * 1000);
 
   return (
     <div className="text-center align-items-center justify-content-center">
       <p className="display-6 mb-3">{t('tokenomics.header')}</p>
-      <p>{t('tokenomics.cycle_duration', { duration: config.cycleDuration / (60 * 60 * 1000) })}</p>
+      <p>
+        {t('tokenomics.cycle_duration', {
+          duration: cycleDuration,
+          cycle_inflection: getWordInflection(cycleDuration, [
+            t('tokenomics.cycle_inflections.one'),
+            t('tokenomics.cycle_inflections.few'),
+            t('tokenomics.cycle_inflections.many'),
+          ]),
+        })}
+      </p>
       <div className="col-lg-4 col-md-6 mx-auto">
         <Table responsive bordered>
           <thead>
