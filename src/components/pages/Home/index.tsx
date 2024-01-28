d03279c147bbeb87e98220a368dbf1d7cd0e735e
sundr17 <sundoor1992@mail.ru>
2024-01-28 18:07:52 +0100

    Added required connect wallet message on homepage

--- src/components/pages/Home/index.tsx
+++ src/components/pages/Home/index.tsx
@@ -5,6 +5,7 @@ import { LinkContainer } from 'react-router-bootstrap';
 import Container from 'react-bootstrap/Container';
 import { Col, Row } from 'react-bootstrap';
 import Button from 'react-bootstrap/Button';
+import { useAccount } from 'wagmi';
 
 import { getBoughtData } from '@/services/data/items';
 import ItemsGrid from '@/components/common/ItemsGrid';
@@ -12,35 +13,40 @@ import TokenomicInfo from '@/components/pages/Home/TokenomicInfo';
 
 export default function Home() {
   const { t } = useTranslation();
+  const { isConnected: isWalletConnected } = useAccount();
   const boughtItems = getBoughtData();
 
   return (
     <Container fluid>
       <Row>
-        <Col sm="4">
-          <TokenomicInfo />
-        </Col>
-        <Col>
-          <ItemsGrid
-            items={boughtItems}
-            emptyPlaceholder={(
-              <div>
-                <p>{t('homepage.grid.empty.header')}</p>
-                <p>
-                  <Trans
-                    i18nKey="homepage.grid.empty.body"
-                    components={{
-                      tokenomicLink: <Link to="tokenomics" />,
-                    }}
-                  />
-                </p>
-                <LinkContainer to="/shop">
-                  <Button>{t('homepage.grid.empty.button')}</Button>
-                </LinkContainer>
-              </div>
-            )}
-          />
-        </Col>
+        {isWalletConnected ? (
+          <>
+            <Col sm="4">
+              <TokenomicInfo />
+            </Col>
+            <Col>
+              <ItemsGrid
+                items={boughtItems}
+                emptyPlaceholder={(
+                  <div>
+                    <p>{t('homepage.grid.empty.header')}</p>
+                    <p>
+                      <Trans
+                        i18nKey="homepage.grid.empty.body"
+                        components={{
+                          tokenomicLink: <Link to="tokenomics" />,
+                        }}
+                      />
+                    </p>
+                    <LinkContainer to="/shop">
+                      <Button>{t('homepage.grid.empty.button')}</Button>
+                    </LinkContainer>
+                  </div>
+                )}
+              />
+            </Col>
+          </>
+        ) : <p className="display-8">{t('homepage.wallet_not_connected')}</p>}
       </Row>
     </Container>
   );
