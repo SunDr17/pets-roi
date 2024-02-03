1c17a1ad7db7e6fef61c865c18982b3b140255fc
sundr17 <sundoor1992@mail.ru>
2024-02-03 22:54:14 +0100

    Added cats

--- src/components/pages/Shop/index.tsx
+++ src/components/pages/Shop/index.tsx
@@ -18,7 +18,10 @@ export default function Shop() {
     >
       {items.map((tabData) => (
         <Tab key={tabData.title} eventKey={tabData.title} title={t(`shop.tabs.${tabData.title}`)}>
-          <ItemsGrid items={tabData.data} rowProps={{ xs: 2, sm: 3, md: 4, lg: 6 }} />
+          <ItemsGrid
+            items={tabData.data.sort((it1, it2) => it1.price - it2.price)}
+            rowProps={{ xs: 2, sm: 3, md: 4, lg: 6 }}
+          />
         </Tab>
       ))}
     </Tabs>
